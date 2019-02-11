8f31c4f2da82adfaeabfd86006340249d8bd4dfb
jaxfire <jamesash@jaxfire.co.uk>
2019-02-11 23:10:07 +0000

    Add SpaceXApiService

---
+++ app/src/main/java/com/jaxfire/spacexinfo/data/SpaceXApiService.kt
@@ -0,0 +1,40 @@
+package com.jaxfire.spacexinfo.data
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.jaxfire.spacexinfo.data.response.LaunchesResponse
+import com.jaxfire.spacexinfo.data.response.RocketsResponse
+import kotlinx.coroutines.Deferred
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+// https://api.spacexdata.com/v3/rockets
+// https://api.spacexdata.com/v3/launches
+
+interface SpaceXApiService {
+
+    @GET("rockets")
+    fun getAllRockets(): Deferred<RocketsResponse>
+
+    @GET("launches")
+    fun getLaunchesForRocket(
+        @Query("rocket_id") rocketId: String
+    ): Deferred<LaunchesResponse>
+
+    companion object {
+        operator fun invoke(): SpaceXApiService {
+
+            val okHttpClient = OkHttpClient.Builder().build()
+
+            return Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("https://api.spacexdata.com/v3/")
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(SpaceXApiService::class.java)
+        }
+    }
+}
