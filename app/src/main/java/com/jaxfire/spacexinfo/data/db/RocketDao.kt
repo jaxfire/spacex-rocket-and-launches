4bfa56a9c768193f368b1dd61c7809bdaa71abec
jaxfire <jamesash@jaxfire.co.uk>
2019-02-12 22:21:38 +0000

    Add non-livedata query to the RocketDao and clear up comments.

--- app/src/main/java/com/jaxfire/spacexinfo/data/db/RocketDao.kt
+++ app/src/main/java/com/jaxfire/spacexinfo/data/db/RocketDao.kt
@@ -13,26 +13,17 @@ import com.jaxfire.spacexinfo.data.network.response.RocketResponse
 interface RocketDao {
 
     @Query("SELECT * FROM rocket_table")
+    fun getAllRocketsNonLive(): List<RocketResponse>
+
+    @Query("SELECT * FROM rocket_table")
     fun getAllRockets(): LiveData<List<RocketResponse>>
 
-    @Query("SELECT COUNT(*) FROM rocket_table")
-    fun getRocketCount(): Int
-
-//    @Query("SELECT * FROM launch WHERE rocket_rocketId = :rocketId")
-//    fun getLaunches(rocketId: String)
-
-    // TODO: Do we need upsert if we are going to delete all data anyway?
-
-    // TODO: Before release. Check if still have no foreign keys in our tables.
-    // REPLACE will work fine as we have no foreign keys in our tables.
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun upsertRocket(rocketEntry: RocketResponse) // TODO: Refactor if we create db entities separate from network model
+    // TODO: Test this out when active filter is implemented
+    @Query("SELECT * FROM rocket_table WHERE active = 1")
+    fun getActiveRockets(): LiveData<List<RocketResponse>>
 
     @Insert
-    fun insertRockets(rocketEntries: List<RocketResponse>) // TODO: Refactor if we create db entities separate from network model
-
-//    @Insert(onConflict = OnConflictStrategy.REPLACE) // TODO: Refactor if we create db entities separate from network model
-//    fun upsertLaunch(launchEntry: LaunchResponse)
+    fun insertRockets(rocketEntries: List<RocketResponse>)
 
     @Query("DELETE FROM rocket_table")
     fun deleteAll()
