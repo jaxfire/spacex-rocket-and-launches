dfdb0ed00078a065edf16a17d739c9efd0f5d812
jaxfire <jamesash@jaxfire.co.uk>
2019-02-12 22:22:41 +0000

    Update dependency injection.

--- app/src/main/java/com/jaxfire/spacexinfo/SpaceXInfoApplication.kt
+++ app/src/main/java/com/jaxfire/spacexinfo/SpaceXInfoApplication.kt
@@ -6,14 +6,12 @@ import com.jaxfire.spacexinfo.data.db.SpaceXInfoDatabase_Impl
 import com.jaxfire.spacexinfo.data.network.*
 import com.jaxfire.spacexinfo.data.repository.SpaceXInfoRepository
 import com.jaxfire.spacexinfo.data.repository.SpaceXInfoRepositoryImpl
+import com.jaxfire.spacexinfo.ui.rocket_info.detail.RocketDetailViewModelFactory
 import com.jaxfire.spacexinfo.ui.rocket_info.list.RocketListViewModelFactory
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
-import org.kodein.di.generic.bind
-import org.kodein.di.generic.instance
-import org.kodein.di.generic.provider
-import org.kodein.di.generic.singleton
+import org.kodein.di.generic.*
 
 
 class SpaceXInfoApplication : Application(), KodeinAware {
@@ -23,10 +21,12 @@ class SpaceXInfoApplication : Application(), KodeinAware {
 
         bind() from singleton { SpaceXInfoDatabase(instance()) }
         bind() from singleton { instance<SpaceXInfoDatabase>().rocketDao() }
+        bind() from singleton { instance<SpaceXInfoDatabase>().launchesDao() }
         bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(instance()) }
         bind() from singleton { SpaceXApiService(instance()) }
         bind<SpaceXInfoNetworkDataSource>() with singleton { SpaceXInfoNetworkDataSourceImpl(instance()) }
-        bind<SpaceXInfoRepository>() with singleton { SpaceXInfoRepositoryImpl(instance(), instance()) }
+        bind<SpaceXInfoRepository>() with singleton { SpaceXInfoRepositoryImpl(instance(), instance(), instance()) }
         bind() from provider { RocketListViewModelFactory(instance()) }
+        bind() from factory { detailRocketId: String -> RocketDetailViewModelFactory(instance(), detailRocketId) }
     }
 }
