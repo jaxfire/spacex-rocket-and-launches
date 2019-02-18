55d5babd7f797270dbd27b7aef1d0557412794aa
jaxfire <jamesash@jaxfire.co.uk>
2019-02-18 00:18:49 +0000

    Refactor lineChart initialisation to use .apply {}

--- app/src/main/java/com/jaxfire/spacexinfo/ui/rocket_info/detail/RocketDetailViewModel.kt
+++ app/src/main/java/com/jaxfire/spacexinfo/ui/rocket_info/detail/RocketDetailViewModel.kt
@@ -29,8 +29,8 @@ class RocketDetailViewModel(
 
     fun updateChart(launchData: List<LaunchEntity>, lineChart: LineChart) {
 
-        val yearsCount = mutableMapOf<String, Int>()
         val listOfYears = launchData.map { it.launchYear }.toMutableList()
+        val yearsCount = mutableMapOf<String, Int>()
         val listOfValidYears = listOfYears.filter { yearStr -> yearStr.toFloatOrNull() != null }
         listOfValidYears.forEach { year -> yearsCount[year] = listOfYears.count { it == year } }
         val entries = mutableListOf<Entry>()
@@ -43,27 +43,31 @@ class RocketDetailViewModel(
         dataSet.valueTextSize = 12f
         dataSet.color = R.color.space_x_blue
         val lineData = LineData(dataSet)
-        lineChart.data = lineData
-        lineChart.setDrawGridBackground(false)
-        lineChart.setDrawBorders(false)
-        lineChart.legend.isEnabled = false
-        val description = Description()
-        description.text = ""
-        lineChart.description = description
-        lineChart.setTouchEnabled(false)
-        lineChart.axisLeft.axisMinimum = 0f
-        lineChart.axisLeft.granularity = 1f
-        lineChart.isDragEnabled = true
-        lineChart.moveViewToX(dataSet.entryCount.toFloat())
-        lineChart.xAxis.setDrawAxisLine(true)
-        lineChart.xAxis.setDrawGridLines(true)
-        lineChart.xAxis.isGranularityEnabled = true
-        lineChart.xAxis.granularity = 1f
-        lineChart.xAxis.labelCount = listOfYears.size
-        lineChart.xAxis.textSize = 10f
-        lineChart.xAxis.valueFormatter = ChartValueFormatter()
-        lineChart.axisLeft.isEnabled = false
-        lineChart.axisRight.isEnabled = false
-        lineChart.invalidate()
+
+        val customDescription = Description()
+        customDescription.text = ""
+
+        lineChart.apply {
+            data = lineData
+            setDrawGridBackground(false)
+            setDrawBorders(false)
+            legend.isEnabled = false
+            description = customDescription
+            setTouchEnabled(false)
+            axisLeft.axisMinimum = 0f
+            axisLeft.granularity = 1f
+            isDragEnabled = true
+            moveViewToX(dataSet.entryCount.toFloat())
+            xAxis.setDrawAxisLine(true)
+            xAxis.setDrawGridLines(true)
+            xAxis.isGranularityEnabled = true
+            xAxis.granularity = 1f
+            xAxis.labelCount = listOfYears.size
+            xAxis.textSize = 10f
+            xAxis.valueFormatter = ChartValueFormatter()
+            axisLeft.isEnabled = false
+            axisRight.isEnabled = false
+            invalidate()
+        }
     }
 }
